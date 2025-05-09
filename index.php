a6f3b077d1007b78aca5f161f16663623a621d6e
Elijah Busch <elijah1busch@gmail.com>
2025-05-09 12:33:19 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head lang="en-US">
+    <title>Elijah's Website V2</title>
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <script src="/script.js"></script>
+    <!-- Google tag (gtag.js) --><script type="text/javascript" async src="https://www.googletagmanager.com/gtag/js?id=G-3WDY84G85Z"></script><script type="text/javascript"> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-3WDY84G85Z'); </script>
+    <link rel="icon" href="Favicon.ico" type="image/x-icon"/>
+    <link href="/style.css" rel="stylesheet" />  
+    <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
+    <script type="text/javascript" defer src="https://pyscript.net/latest/pyscript.js"></script>
+</head>
+<body>
+    <?php
+    echo "hello world"
+    ?>
+</body>
+</html>
