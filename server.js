e8e26076987748133c1d99481902c613e6074894
Elijah Busch <elijah1busch@gmail.com>
2025-09-12 01:13:56 +0000

    Automated commit: 2025-09-12 01:13:56

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 });
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
 
 app.listen(port, () => {
